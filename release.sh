0e5074c1a1bf84cb441a7e942fae3a30214abc12
s.pyzhov <s.pyzhov@corp.mail.ru>
2018-04-23 15:11:54 +0300

    add `dependencies` block

--- release.sh
+++ release.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 
-if [[ "$1" == "" ]]; then
-LEVEL=patch
-else
-LEVEL=$1
-fi
-
-npm version ${LEVEL}
-VERSION=$(jq -r ".version" package.json)
-git add ./package.json
-git commit -m "Increase version"
-git push
-git tag $(VERSION)
-git push --tags
+npm version patch
 npm publish
